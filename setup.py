359768d82d037cfd239875f47f545e15953f24ee
killf <killf@foxmail.com>
2019-10-23 09:41:26 +0800

    init

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ImageSR",
+    version="1.0",
+    keywords=("SR", "Image", "Super-Resolution"),
+    description="A toolkit of image super-resolution for training or inference.",
+    long_description="A toolkit of image super-resolution for training or inference.",
+    license="Apache License 2.0",
+    url="https://github.com/killf/ImageSR",
+    author="killf",
+    author_email="killf@foxmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=['numpy', 'paddlepaddle-gpu', 'opencv-python'],
+    scripts=[],
+    entry_points={}
+)
---
+++ src/metrics.py
@@ -0,0 +1,15 @@
+import numpy as np
+import math
+
+__all__ = ['psnr']
+
+
+# psnr=10*log10((2^n-1)^2/mse)
+def psnr(im1, im2, n=8):
+    im1 = im1.astype('float32')
+    im2 = im2.astype('float32')
+
+    mse = np.mean((im1 - im2) ** 2)
+    if mse < 1.0e-10:
+        return 100
+    return 10 * math.log10((pow(2, n) - 1.0) ** 2 / mse)
